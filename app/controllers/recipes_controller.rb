6e277ab73f75f96f3fa2078b6d2af2125a99d18f
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-30 16:43:39 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,23 +9,20 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    @recipe.ingredients.build
-    @recipe.ingredients.build
+    @recipe.ingredients.build(name: 'one')
+    @recipe.ingredients.build(name: 'two')
   end
 
   def create
-    @recipe = Recipe.new(recipe_params)
-    redirect_to recipes_path
+    @recipe = Recipe.create(recipe_params)
+     redirect_to recipes_path
   end
 
-  private
-
   def recipe_params
-      params.require(:recipe).permit(
-        :title,
-        ingredients_attributes: [
-          :name,
-          :quantity
-        ] )
- end
-end
+       params.require(:recipe).permit(
+         :title,
+         ingredients_attributes: [
+           :name,
+           :quantity
+         ] )
+  end
