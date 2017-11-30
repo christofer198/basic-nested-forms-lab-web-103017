0d5315e6c127deeb3ad3fc4cdb7726cb201ac743
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-30 16:45:19 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,7 +15,7 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.create(recipe_params)
-     redirect_to recipes_path
+    redirect_to recipes_path
   end
 
   def recipe_params
